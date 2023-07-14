38e0dfb4af071aa93dc653f9735e05a17934ba7d
DinaraDervel <dinaradervel@gmail.com>
2023-07-14 15:48:40 -0600

    add processing of checking checkbox

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('.form');
 const inputTask = document.querySelector('#todo');
-const buttonAdd = document.querySelector('#add-task');
 const list = document.querySelector(".toDoList");
 const buttonClear = document.querySelector('#clear-list');
 let arrayOfTasks = [];
@@ -37,7 +36,10 @@ buttonClear.addEventListener('click', () => {
 list.addEventListener('click', (e) => {
     let id = e.target.getAttribute('name');
     if (!id) return;
-    else arrayOfTasks.forEach(el => { if (el.id === Number(id)) el.checked = el.checked ? false : true; });
+    else {
+        arrayOfTasks.forEach(el => { if (el.id === Number(id)) el.checked = el.checked ? false : true; });
+        window.localStorage.setItem('tasks', JSON.stringify(arrayOfTasks));
+    }
 });
 
 const createTask = () => {
@@ -50,6 +52,9 @@ const createTask = () => {
 
 const addTaskToDOM = (task) => {
     const div = document.createElement("div");
-    div.innerHTML = `<div><input type='checkbox' name='${task.id}' /><label for="${task.id}">${task.name}</label ></div > `;
+    div.innerHTML = `<input type='checkbox' name='${task.id}'/><label for="${task.id}">${task.name}</label >`;
     list.appendChild(div);
+    const checkbox = document.getElementsByName(task.id);
+    checkbox[0].checked = task.checked;
+    if (checkbox.checked) checkbox[0].classList.add('input:checked');
 };
